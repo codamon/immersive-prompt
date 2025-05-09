ce915545e281da83b119d535bd92d416764d0f70
CoDamon <codamon@users.noreply.github.com>
2025-05-09 22:04:26 +1200

    build: configure vite and tailwind settings

--- src/options/main-options.tsx
+++ src/options/main-options.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../assets/global.css'
+import '../styles/tailwind.css'
 import OptionsApp from './OptionsApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
